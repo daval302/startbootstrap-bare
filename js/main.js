19b97962e82c51486096a26e7dcb288c83151874
daval302 <daval302@gmail.com>
2018-04-28 11:30:42 +0100

    navigator on templateUrl

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ var app = angular.module('bare', []);
 
 app.controller('main', function($scope){
 	$scope.me = "daval302";
+}).directive('navigator',function(){
+	return {templateUrl: 'pages/navigator.html'}
 });

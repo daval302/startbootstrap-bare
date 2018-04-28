dbacb27abb3ac1f6854d8c21e9ed9254a9a58452
daval302 <daval302@gmail.com>
2018-04-28 11:35:29 +0100

    added content page as templateUrl

--- js/main.js
+++ js/main.js
@@ -4,4 +4,6 @@ app.controller('main', function($scope){
 	$scope.me = "daval302";
 }).directive('navigator',function(){
 	return {templateUrl: 'pages/navigator.html'}
+}).directive('content',function(){
+	return {templateUrl: 'pages/content.html'}
 });

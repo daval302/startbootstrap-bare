0d2240c9ecfff5933f36dee7aefd6ad6fd16fc25
daval302 <daval302@gmail.com>
2018-05-02 10:54:37 +0100

    content in a form of object key-value to handled in angularjs

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ var app = angular.module('bare', []);
 
 app.controller('main', function($scope){
 	
-	$scope.navigator = {'Home': 'active', 'About': null, 'Services': null, 'Contact': null};
+	$scope.navigator = {
+		'Home': 'active', 
+		'About': null, 
+		'Services': null, 
+		'Contact': null
+	};
+
+	$scope.content = {
+		title: 'A Bootstrap 4 Starter Template',
+		description: 'Complete with pre-defined file paths and responsive navigation!',
+		dependencies: ['Bootstrap 4.0.0', 'jQuery 3.3.0', 'Angular 1.6.10']
+	}
+
 
 }).directive('navigator',function(){
 	return {templateUrl: 'pages/navigator.html'}
